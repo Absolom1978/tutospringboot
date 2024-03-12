2c8b3374902edf866f80f20b70320cfa2be2edcf
Absolom1978 <pcarpentierpro@gmail.com>
2024-03-12 10:03:13 +0100

    initialisation du projet

---
+++ src/main/java/com/pcarpentier/tutospringboot/TutospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pcarpentier.tutospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcarpentier/tutospringboot/TutospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcarpentier.tutospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
